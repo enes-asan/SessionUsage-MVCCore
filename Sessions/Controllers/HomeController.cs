7276e61801270a570160783645080049f2d2c7d7
Enes ASAN <82983724+easan60@users.noreply.github.com>
2021-09-19 14:52:21 +0300

    datas

---
+++ Sessions/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Sessions.Entites;
+using Sessions.ExtensionMethots;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sessions.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string Index()
+        {
+            //String and int
+            HttpContext.Session.SetInt32("ProductCount", 12);
+            HttpContext.Session.SetString("ProductName", "PC");
+
+            //Extension string and int, Object Sesion
+            HttpContext.Session.SetObject("obj", new Product { Id = 1, ProductCount = 11, ProductName = "Keyboard" });
+
+            return "session created";
+        }
+        public string Index2()
+        {
+            return string.Format("Product Name={0},Product Count={1},ObjectListProductName={2},ObjectListProductCount={3}", 
+                HttpContext.Session.GetString("ProductName"), 
+                HttpContext.Session.GetInt32("ProductCount"),
+                HttpContext.Session.GetObject<Product>("obj").ProductName,
+                HttpContext.Session.GetObject<Product>("obj").ProductCount
+                );
+        }
+  
+    }
+}
---
+++ Sessions/ExtensionMethots/SessionExtensionMethots.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Sessions.ExtensionMethots
+{
+    public static class SessionExtensionMethots
+    {
+        public static void SetObject(this ISession session,string key,object value)
+        {
+            var stringValue = JsonConvert.SerializeObject(value);
+            session.SetString(key, stringValue);
+        }
+        public static T GetObject<T>(this ISession session,string key) where T:class
+        {
+            var stringValue = session.GetString(key);
+            if (string.IsNullOrEmpty(stringValue))
+            {
+                return null;
+            }
+            T objectValue = JsonConvert.DeserializeObject<T>(stringValue);
+            return objectValue;
+        }
+    }
+}
